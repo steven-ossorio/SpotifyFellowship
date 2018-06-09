b73061a513ffa4e9facfac75cedeaab78c2e931a
Steven <codebusiness2014@gmail.com>
2018-06-09 08:13:42 -0400

    Finished question 2

--- question3.js
+++ question3.js
@@ -1,4 +1,4 @@
-// Question 3 -- changePossibilities(amount,amount): Your quirky boss collects rare, old coins. 
+// Question 3 -- changePossibilities(amount,amount): Your quirky boss collects rare, old coins.
 // They found out you're a programmer and asked you to solve something they've been wondering for a long time.
 //
 // Write a function that, given an amount of money and an array of coin denominations, computes
@@ -11,3 +11,24 @@
 // 1¢, 1¢, 2¢
 // 1¢, 3¢
 // 2¢, 2¢
+
+function changePossibilities(amount, denominations) {
+  let numberOfWaysNCent = new Array(amount + 1).fill(0);
+  numberOfWaysNCent[0] = 1;
+
+  denominations.forEach( coin => {
+    for (let currentCoin = coin; currentCoin <= amount; currentCoin++) {
+      let remainder = currentCoin - coin;
+      numberOfWaysNCent[currentCoin] += numberOfWaysNCent[remainder];
+    }
+  });
+
+
+  return numberOfWaysNCent[amount];
+}
+
+let a1 = 4,
+    d1 = [1, 2, 3],
+    test1 = changePossibilities(a1, d1);
+
+console.log(test1);
